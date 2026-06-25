24edbf2e911e2752f530d3dbcd66169be9745e63
Jason2Car <jasonshuncheng@gmail.com>
2026-06-25 16:29:13 -0500

    Slightly editted kaufman_to_monoid

--- src/Computation/kaufman_to_monoid.py
+++ src/Computation/kaufman_to_monoid.py
@@ -1,4 +1,3 @@
-import math
 def kaufman_to_monoid(
     kauf_state : str,
     numStrands : int
@@ -8,16 +7,16 @@ def kaufman_to_monoid(
     temp_lieb = [] 
 
     index = len(kauf_state) - 1
-    element = numStrands - 2
+    element = numStrands - 1 - 1
 
     while index >= 0:
         if(kauf_state[index] == '1'): #if 1
-            temp_lieb.append(element % (numStrands - 1) + 1)
+            temp_lieb.append(element + 1)
         index -= 1
-        element -= 1
+        element = (element - 1) % (numStrands - 1)
         
     return temp_lieb 
 
 
-
+print(kaufman_to_monoid("1010000", 3))
 
